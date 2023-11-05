59fe9854a5200c979c081aecb65ea46f31f94fb0
emilio rivas <emiliorivasruiz@gmail.com>
2023-11-05 03:19:23 -0500

    docker img

--- src/server.js
+++ src/server.js
@@ -1,58 +1,10 @@
-// const express = require("express");
-// const app = express();
-// const fs = require("fs");
-// const PORT = process.env.PORT || 3000;
-
-// // paste your IP address of the local network
-// // const HOST = "192.168.0.5";
-
-// // specify the self-signed certificate for https connection
-// const options = {
-//   key: fs.readFileSync("key.pem"),
-//   cert: fs.readFileSync("cert.pem")
-// };
-// const server = require("https").createServer(options, app);
-// const io = require("socket.io")(server);
-
-// app.use(express.static("public"));
-
-// var socketRoom;
-
-// io.on("connection", socket => {
-//   console.log("user connected to the socket");
-
-//   socket.on("room", room => {
-//     io.emit("room", room);
-//   });
-
-//   socket.on("join", room => {
-//     socketRoom = room;
-//     socket.join(socketRoom);
-//     socket.to(socketRoom).emit("join", room);
-//   });
-
-//   socket.on("offer", offer => {
-//     socket.to(socketRoom).emit("offer", offer);
-//   });
-
-//   socket.on("answer", answer => {
-//     socket.to(socketRoom).emit("answer", answer);
-//   });
-
-//   socket.on("candidate", candidate => {
-//     socket.to(socketRoom).emit("candidate", candidate);
-//   });
-// });
-
-// server.listen(PORT, () => {
-//   console.log("Server is listening: ", PORT);
-// });
-
 const express = require("express");
 const app = express();
-const fs = require("fs");
-const http = require("http").createServer(app); // Usamos http en lugar de https
+const http = require("http").createServer(app);
 const io = require("socket.io")(http);
+const cors = require('cors');
+
+app.use(cors());
 
 
 app.use(express.static("public"));
@@ -67,7 +19,7 @@ router.get("/", (req, res) => {
 
 app.use(router);
 
-var socketRoom;
+let socketRoom;
 
 const users = [];
 
